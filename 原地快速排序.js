f3e1edc5c412a94ac12e8610c46dfe0b52b2016f
rookiexbf <1336400676@qq.com>
2021-07-15 20:23:19 +0800

    republish

--- 原地快速排序.js
+++ 原地快速排序.js
@@ -5,6 +5,7 @@ function quickSort(list,leftIndex,rightIndex){
   let maxIndex = minIndex+1;
   if(leftIndex >= rightIndex) return
   for(let i = leftIndex+1;i<=rightIndex;i++){
+  debugger
     if(list[i]>basic){
       maxIndex++
     }else{
@@ -15,12 +16,13 @@ function quickSort(list,leftIndex,rightIndex){
       maxIndex++
     }
   }
+  debugger
   let tem = list[leftIndex] 
   list[leftIndex] = list[minIndex]
   list[minIndex] =tem
-  quickSort(list,leftIndex,minIndex)
+  quickSort(list,leftIndex,minIndex-1)
   quickSort(list,minIndex+1,rightIndex)
 }
-let a = [15,3,4,5,1,2,6,7,8,13]
+let a = [10,3,4,5,1,2,6,7,8,13]
 quickSort(a,0,a.length-1)
 console.log(a)
